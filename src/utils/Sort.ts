8c307643e4b020924aa0b3d3ce49fb2ab7b3a707
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-07-27 10:00:31 +0200

    add: start logic

--- src/utils/Sort.ts
+++ src/utils/Sort.ts
@@ -1,6 +1,28 @@
 import { Player } from "../types/Player";
 
+export const mockPlayers: Player[] = [
+  { id: 1, name: "Magnus Carlsen", elo: 2850 },
+  { id: 2, name: "Hikaru Nakamura", elo: 2780 },
+  { id: 3, name: "Alireza Firouzja", elo: 2765 },
+  { id: 4, name: "Fabiano Caruana", elo: 2800 },
+  { id: 5, name: "Ian Nepomniachtchi", elo: 2785 },
+]
+
 function sort(players: Player[]): Player[] {
+  let pileIncreasingOrder: Player[] = [];
+  let pileIntruder:Player[] = [];
+
+  let lastElo = players[0].elo;
+    for (let i = 0; i < players.length; i++) {
+      const player = players[i];
+      if (player.elo >= lastElo) {
+      pileIncreasingOrder.push(player);
+      lastElo = player.elo;
+    } else {
+      pileIntruder.push(player);
+    }
+    }
+
   return [...players];
 }
 export default sort
